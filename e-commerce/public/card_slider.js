8be41541e0a3aa588cb7369acfd6e03a327c556a
AMAN KUMAR CHHARI <140314215+chhari07@users.noreply.github.com>
2024-06-16 09:08:54 +0530

    Add files via upload

---
+++ e-commerce/public/card_slider.js
@@ -0,0 +1,43 @@
+
+	function swipeCards() {
+  return {
+  cards: [
+    {
+      id: 1,
+      image: `https://m.media-amazon.com/images/I/31gxNAZMXbL._SX300_SY300_QL70_FMwebp_.jpg?${encodeURIComponent('')}`,
+      title: 'Skullcandy  ',
+      description: '2 On-Ear Wireless Headphones, 34 Hr Battery, Microphone, Works with iPhone Android and Bluetooth Devices - Black.',
+      price: 8.99,
+
+      
+    },
+    {
+      id: 2,
+      image: `https://m.media-amazon.com/images/I/51vjytwuzNL._SX569_.jpg${encodeURIComponent('')}`,
+      title: 'OnePlus 12R',
+      description: 'EvoFox Shadow Optical USB Wired Gaming Mouse with Upto 3600 DPI Gaming Sensor | 6 Buttons Design | 7 Million Clicks .',
+      price: 469,
+     
+    },
+    {
+      id: 3,
+      image: `https://m.media-amazon.com/images/I/61+48zFNZ7L._SX679_.jpg?${encodeURIComponent('Iced Coffee')}`,
+      title: 'LG Electronics Ultragear 21:9 Curved Gaming LED Monitor',
+      description: 'Roll over image to zoom in LG Electronics Ultragear 21:9.',
+      price: 4000.99,
+  
+    },
+    {
+      id: 4,
+      image: `https://cdns3.thecosmicbyte.com/wp-content/uploads/Main-Image-2.jpg.webp${encodeURIComponent('')}`,
+      title: ' Cosmic Byte CB-GK-25 ',
+      description: 'Cosmic Byte CB-GK-25 Pandora TKL Mechanical Keyboard Upgraded with Swappable Outemu Blue Switches and Rainbow LED (Black/Grey) ',
+      price: 70.99,
+
+    },
+   
+    
+  ],
+
+};
+}
